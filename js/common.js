d343e49531022a56208c738279bab8ab116443d5
vkrotkikh <krotkikhvadim@gmail.com>
2018-01-31 15:21:02 +0200

    add new pages

--- js/common.js
+++ js/common.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	// Scroll header
+
 	$(window).on('scroll', function() {
 		if ($(window).scrollTop() > 10) {
 			$('.header').addClass('-scroll');
@@ -8,11 +10,18 @@ $(document).ready(function() {
 		}
 	});
 
+
+	// Custom navbar
+
 	$('.navbar-toggler.-custom').click(function(){
 		$(this).toggleClass('-show-menu');
 		$('.header').toggleClass('-show-menu');
 		$('.language').toggleClass('-show-menu');
 	});
+
+
+	// Home slider
+
 
 	$('.home-slider').slick({
 		slidesToShow: 1,
@@ -22,5 +31,21 @@ $(document).ready(function() {
 		nextArrow: '<div class="slider-next-arrow"><i class="icon-down-arrow"></i></div>',
 		prevArrow: '<div class="slider-prev-arrow"><i class="icon-up-arrow"></i></div>',
 	});
+
+
+	// Open artiset about information
+
+	if($('.-JS_open_acc_about').length){
+
+		var scrollTopToBodySection = $('.account-body').offset().top;
+
+		$('.account-about').css('top', scrollTopToBodySection +'px');
+
+
+		$('.-JS_open_acc_about').on('click' , function(){
+			$(this).parents('.account-about').toggleClass('-open');
+		});
+
+	}
 
 });
