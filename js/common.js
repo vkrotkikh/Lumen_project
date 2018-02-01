8111d37a6d4dacff1c5bc2ad9df362c286a202a8
vitalikcolt <vitalikcolt@gmail.com>
2018-02-01 12:49:17 +0200

    collection-page added

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 	// Scroll header
-
 	$(window).on('scroll', function() {
 		if ($(window).scrollTop() > 10) {
 			$('.header').addClass('-scroll');
@@ -10,43 +9,46 @@ $(document).ready(function() {
 		}
 	});
 
+	// Scroll to Top
+	$('.up-btn').on('click', function() {
+		$('html, body').animate({ scrollTop: 0 }, 1000);
+		return false;
+	});
 
 	// Custom navbar
-
 	$('.navbar-toggler.-custom').click(function(){
 		$(this).toggleClass('-show-menu');
 		$('.header').toggleClass('-show-menu');
 		$('.language').toggleClass('-show-menu');
 	});
 
-	$('.custom-scroll').mCustomScrollbar({
-		scrollbarPosition:"outside",
-		scrollButtons:{ enable: true }
-	});
-	// Home slider
-	$('.home-slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		vertical: true,
-		arrows: true,
-		nextArrow: '<div class="slider-next-arrow"><i class="icon-down-arrow"></i></div>',
-		prevArrow: '<div class="slider-prev-arrow"><i class="icon-up-arrow"></i></div>',
-	});
+	// Custom Scroll
+	if($('.custom-scroll').length){
+		$('.custom-scroll').mCustomScrollbar({
+			scrollbarPosition:"outside",
+			scrollButtons:{ enable: true }
+		});
+	}
 
+	// Home slider
+	if($('.home-slider').length){
+		$('.home-slider').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			vertical: true,
+			arrows: true,
+			nextArrow: '<div class="slider-next-arrow"><i class="icon-down-arrow"></i></div>',
+			prevArrow: '<div class="slider-prev-arrow"><i class="icon-up-arrow"></i></div>',
+		});
+	}
 
 	// Open artiset about information
-
 	if($('.-JS_open_acc_about').length){
-
 		var scrollTopToBodySection = $('.account-body').offset().top;
-
 		$('.account-about').css('top', scrollTopToBodySection +'px');
-
-
 		$('.-JS_open_acc_about').on('click' , function(){
 			$(this).parents('.account-about').toggleClass('-open');
 		});
-
 	}
 
 });
